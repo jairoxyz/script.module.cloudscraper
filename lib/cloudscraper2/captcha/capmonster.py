56a8436ce8997e6c2edcbe7c6558cd4bc4abcdfb
jairoxyz <jairoxyz@noreply.com>
2022-06-18 11:54:10 +0200

    update to upstream 1.2.60

--- lib/cloudscraper2/captcha/capmonster.py
+++ lib/cloudscraper2/captcha/capmonster.py
@@ -36,9 +36,7 @@ class captchaSolver(Captcha):
     def checkErrorStatus(response):
         if response.status_code in [500, 502]:
             raise CaptchaServiceUnavailable(
-                'CapMonster: Server Side Error {}'.format(
-                    response.status_code
-                )
+                f'CapMonster: Server Side Error {response.status_code}'
             )
 
         payload = response.json()
@@ -68,7 +66,7 @@ class captchaSolver(Captcha):
 
         response = polling2.poll(
             lambda: self.session.post(
-                '{}/getTaskResult'.format(self.host),
+                f'{self.host}/getTaskResult',
                 json={
                     'clientKey': self.clientKey,
                     'taskId': taskID
@@ -103,19 +101,19 @@ class captchaSolver(Captcha):
             'task': {
                 'websiteURL': url,
                 'websiteKey': siteKey,
-                'softId': 37,
                 'type': 'NoCaptchaTask' if captchaType == 'reCaptcha' else 'HCaptchaTask'
-            }
+            },
+            'softId': 37
         }
 
         if self.proxy:
             data['task'].update(self.proxy)
         else:
-            data['task']['type'] = '{}Proxyless'.format(data['task']['type'])
+            data['task']['type'] = f"{data['task']['type']}Proxyless"
 
         response = polling2.poll(
             lambda: self.session.post(
-                '{}/createTask'.format(self.host),
+                f'{self.host}/createTask',
                 json=data,
                 allow_redirects=False,
                 timeout=30
@@ -178,12 +176,12 @@ class captchaSolver(Captcha):
             except polling2.TimeoutException:
                 raise CaptchaTimeout(
                     "CapMonster: Captcha solve took to long and also failed "
-                    "reporting the task with task id {}.".format(taskID)
+                    f"reporting the task with task id {taskID}."
                 )
 
             raise CaptchaTimeout(
                 "CapMonster: Captcha solve took to long to execute "
-                "task id {}, aborting.".format(taskID)
+                f"task id {taskID}, aborting."
             )
 
 
